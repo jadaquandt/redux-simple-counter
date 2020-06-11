d59d3778481e569c38e59555ebfcefdc88afbf05
Jada Quandt <jadaquandt@gmail.com>
2020-06-11 17:37:11 -0400

    finished exercise

--- src/index.js
+++ src/index.js
@@ -9,4 +9,29 @@ subscribe(() => render(getState()))
 
 // Dispatch the "INCREMENT" action every time the +1 button is pressed
 const incrementButton = document.getElementById('increment');
-incrementButton.addEventListener('click', e => dispatch({ type: "INCREMENT" }));
+incrementButton.addEventListener('click', e => dispatch({ type: "INCREMENT", payload: 1 }));
+
+//1) Add a decrement button that decreases the counter by 1 when clicked.
+const decrementButton = document.getElementById('decrement');
+decrementButton.addEventListener('click', e => dispatch({ type: "DECREMENT", payload: 1 }));
+
+//2) Add a +5 button and a -5 button.
+const decrement5Button = document.getElementById('decrement5');
+decrement5Button.addEventListener('click', e => dispatch({ type: "DECREMENT", payload: 5 }));
+
+const increment5Button = document.getElementById('increment5');
+increment5Button.addEventListener('click', e => dispatch({ type: "INCREMENT", payload: 5 }));
+
+//3) Add a dropdown with the values "red", "green", and "blue". Make it change the color of the counter (You'll have to make your own "color" property in the state!)
+const colorSelect = document.getElementById('color');
+colorSelect.addEventListener('change', (e) => {
+    const color = e.target.value;
+    dispatch({ type: 'CHANGE_COLOR', payload: color });
+});
+
+//4) Add a text field that lets the user set the counter value to whatever number they enter
+const numberInput = document.getElementById('userNumber');
+numberInput.addEventListener('change', (e) => {
+    const number = e.target.value;
+    dispatch({ type: 'CHANGE_NUMBER', payload: number })
+});
--- src/reducer.js
+++ src/reducer.js
@@ -1,13 +1,34 @@
 const initialState = {
-    value: 0
+    value: 0,
+    color: 'black'
 }
 
 const reducer = (state = initialState, action) => {
-    let { type } = action;
-    if (type === "INCREMENT") {
+    let { type, payload } = action;
+    if (type === "INCREMENT") { 
         return {
-            value: state.value + 1
-        }
+            ...state,
+            value: state.value + payload
+        };
+    }
+    //1) Add a decrement button that decreases the counter by 1 when clicked.
+    if (type === "DECREMENT") {
+        return {
+            ...state,
+            value: state.value - payload
+        };
+    }
+    if (type === "CHANGE_COLOR") {
+        return {
+            ...state,
+            color: payload
+        };
+    }
+    if (type === "CHANGE_NUMBER") {
+        return {
+            ...state,
+            value: parseInt(payload)
+        };
     }
     return state
 }
